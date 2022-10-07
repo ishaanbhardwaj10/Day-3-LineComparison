1a40337b4e589987ce3407920d43231c23afe379
ishaanbhardwaj10 <ishaanbhardwaj410@gmail.com>
2022-10-07 13:59:32 +0530

    added UC-3 option to switch case in main method

--- Program.cs
+++ Program.cs
@@ -7,6 +7,7 @@
             Console.WriteLine("Welcome to Line Comparison Computation Program");
             Console.WriteLine("Use Case #1: Model a Line and Calculate Length");
             Console.WriteLine("Use Case #2: Check Equality of Two Lines");
+            Console.WriteLine("Use Case #3: Compare Two Lines");
 
             Console.Write("Choose a use case number to run the program: ");
             int choice = Convert.ToInt32(Console.ReadLine());
@@ -18,6 +19,9 @@
                     break;
                 case 2:
                     CheckEquality.TwoLinesEquality();
+                    break;
+                case 3:
+                    CompareTwoLines.TwoLinesComparison();
                     break;
                 default:
                     Console.WriteLine("Please enter a valid use case number!");
